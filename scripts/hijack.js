21244b1b530581b51ca80455878e575f79d3116e
Andrew Michel <aamichel@csu.fullerton.edu>
2017-02-22 20:21:17 -0800

    edited hijack.js

--- scripts/hijack.js
+++ scripts/hijack.js
@@ -1,4 +1,23 @@
-//disables default click functionallity 
-addEventListener('click', function(event) {
-    event.preventDefault();
-});
+var DOCUMENT_LINKS = '.r > a';
+
+function addLinkClickHandler(link) {
+    'use strict';
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+    });
+}
+
+function getLinksArray() {
+    'use strict';
+    var links = document.querySelectorAll(DOCUMENT_LINKS);
+    var linksArr = [].slice.call(links);
+    return linksArr;
+}
+
+function initializeEvents() {
+    'use strict';
+    var links = getLinksArray();
+    links.forEach(addLinkClickHandler);
+}
+
+initializeEvents();
